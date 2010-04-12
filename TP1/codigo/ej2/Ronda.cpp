709145b8b72ec869f1841d35fc774d6d1a838475
pablo.brusco <pablo.brusco@2f5f89bf-aa3b-9c9d-ef5c-1903fc5e024b>
2010-04-12 22:31:21 +0000

    casi casi

--- TP1/codigo/ej2/Ronda.cpp
+++ TP1/codigo/ej2/Ronda.cpp
@@ -2,100 +2,92 @@
 
 
 Ronda::Ronda(){
-	amistades = new map<chica,list<chica> >;
-	enRonda   = new set<chica>;
+	gente = new list<chica>;
+	enRonda   = new set<int>;
 }
 
 Ronda::~Ronda(){
-	delete amistades;
+	delete gente;
 	delete enRonda;
 }
 
-void Ronda::agregarAmistades(chica c,list<chica> amigas){
-	amistades->insert(pair<chica, list<chica> >(c,amigas) );
-}
 
 void Ronda::cargarAmistades(istream& is,int n){
-	
+	gente->clear();	
 	enRonda->clear();
-	amistades->clear();
-	
+	chica c;
 	int m,a;
-	list<chica> amigas;
+	
 	
 	for(int i = 1;i <= n; i++){
 		is >> m;
-		amigas.clear();
+		
+		c.nombrar(i);
+		
+		
 		for(int j = 1; j <= m; j++)
 		{
 			is >> a;
-			amigas.push_front(a);
+			c.amigar(a);
 		}
-		agregarAmistades(i,amigas);
-	}	
-
-//	for(int i = 1;i <= n; i++){
-//	(*amistades)[i].sort(comparacionPorAmistades);
-//	}	
+		
+		gente->push_front(c);
 	
+	}	
+	
+	gente->sort();
+	
+	
+	
+}
+
+bool operator<(const chica &c1,const chica &c2){
+
+return (c1.dameAmigas())->size() < (c2.dameAmigas())->size();
 
 }
-/*
-bool Ronda::comparacionPorAmistades(const chica& primera,const chica& segunda)
-{
-return ((*amistades)[primera].size() > (*amistades)[segunda].size()) ;
-}
-*/
-
-
 
 bool Ronda::resolver(){
 
 	/***************************************************************|
 	|		Comienzo eligiendo la chica con menos amigas			|
-	|		para maximizar el tamaño del arbol jeje					|
+	|		para minimizar el tamaño del arbol 						|
 	****************************************************************/
 
-	chica simpatica = 1;
-	for(int i = 1;i<=amistades->size();i++)
-	{
-		if( (*amistades)[simpatica].size() < (*amistades)[i].size())
-			simpatica = i;
-	}
+	chica solitaria = gente->front();
 
+	
 // la inserto en la ronda y comienzo con el backtracking:
 
-	enRonda->insert(simpatica);
+	enRonda->insert(solitaria.dameNombre());
 	
-	return  probarDistintasRondas(simpatica,simpatica);;
+	return  probarDistintasRondas(solitaria,solitaria);
 
 }
 
-bool Ronda::probarDistintasRondas(chica prim, chica ult){
+bool Ronda::probarDistintasRondas(const chica &prim, const chica &ult){
 
-list<chica>::iterator it,itA,itB;	
+list<chica>::iterator it;
 
-itA = (*amistades)[ult].begin();
-itB =  (*amistades)[ult].end();
-
-	if ((find(itA,itB, prim) != itB) && (amistades->size() == enRonda->size())) {
+	if (((ult.dameAmigas())->count(prim.dameNombre())!=0) && (gente->size() == enRonda->size())) {
 		return true;
 	}
 	else {
 
 		bool res;
 	
-		for (it = (*amistades)[ult].begin();it != (*amistades)[ult].end();it++){
-
-			if (enRonda->count(*it) == 0){
-				enRonda->insert(*it);
+		for (it = gente->begin();it != gente->end();it++){
+		
+			if (enRonda->count(it->dameNombre()) == 0 && (ult.dameAmigas())->count(prim.dameNombre()) != 0){
+				
+				enRonda->insert(it->dameNombre());
 				res = probarDistintasRondas(prim,*it);
 			
 				if(res){
 					return true;
 				}
 				
-				enRonda->erase(*it);
+				enRonda->erase(it->dameNombre());
 				
 			}	
 		}
@@ -104,26 +96,56 @@ itB =  (*amistades)[ult].end();
 
 }
 
-void Ronda::mostrar(ostream& os) const{
 
-	map<chica,list<chica> >::iterator it;
-	list<chica>::iterator it2;
-		
-	for(it=amistades->begin(); it != amistades->end(); it++){
-		os << "Chica: " << (*it).first << "    Amigas: ";
-		for(it2=((*it).second).begin(); it2 != ((*it).second).end(); it2++){
-			os << (*it2) << " ";
-		}
-		os << endl;
+
+
+
+
+///CLASEEEEEEEEEEEEEEEEEEEEE:
+
+
+
+	chica::chica(){
+	amigas = new set<int>;
+	nombre = 0;
 	}
-	os << endl;
-}
+	
+	chica::~chica(){
+	delete amigas;
+
+	}
+	
+	int chica::dameNombre() const{
+	return nombre;
+	}
+	
+	set<int>* chica::dameAmigas() const{
+	return amigas;
+	}
+	
+	chica::chica(const chica &c){
+		nombre = c.dameNombre();
+		amigas = new set<int>;
+		(*amigas)(*(c.dameAmigas()));
+//		amigas((c.dameAmigas())->begin(),(c.dameAmigas())->end()); 
+		}
+
+	chica& chica::operator=(const chica &c){
+	amigas->clear();
+	nombre = c.dameNombre();
+	(*amigas)(*(c.dameAmigas()));
+	//	amigas((c.dameAmigas())->begin(),(c.dameAmigas())->end()); 
+	}
 
 
-ostream& operator<<(ostream& os, const Ronda &r){
-	r.mostrar(os);
-	return os;
-}
+	void chica::nombrar(int n){
+	nombre = n;
+	}
+
+	void chica::amigar(int a){
+	amigas->insert(a);
+	
+	}
 
 
 
