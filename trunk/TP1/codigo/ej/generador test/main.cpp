ef1cffd54da2f9621a2ca114eb26a3731dc7d9a0
pablo.brusco <pablo.brusco@2f5f89bf-aa3b-9c9d-ef5c-1903fc5e024b>
2010-04-13 15:19:41 +0000

    corregidos problemitas, el generador funciona (correr a.out)

--- trunk/TP1/codigo/ej/generador test/main.cpp
+++ trunk/TP1/codigo/ej/generador test/main.cpp
@@ -11,20 +11,36 @@ using namespace std;
 
 
 int main(){
+
 	int seed = 2;
+	int m;
+	int porcentaje = 0;
 	ofstream salida;
-	salida.open("grupoPeleador");
+	cout << endl<<"Ingrese porcentaje y ronda mas grande (desde 2)" << endl;
+	
+	while(porcentaje > 100 or porcentaje<=0){
+	cout << endl<< "porcentaje: ";
+	cin >> porcentaje;
+	}
+
+	cout << endl<< "maxChicas: ";
+	cin >> m;
+	
+
+
+	
+	salida.open("entradaP");
 	srand( seed );
 	seed++;
 	
 	
-	for (int i = 2;i<= 100;i++){
+	for (int i = 2;i<= m;i++){
 	
 		salida << "   " << i << endl;
 		int contador = 0;
 		map<int,set<int> > chicas;
 	
-	int cantRelaciones = ((i-4)*(i-5))/2; //mientras mas chico este numero, menos relaciones por lo tanto menos chances de que haya sol
+	int cantRelaciones = ((i*(i-1)/2)*porcentaje)/100; //mientras mas chico este numero, menos relaciones por lo tanto menos chances de que haya sol
 	
 		while(contador <= cantRelaciones){
 		
@@ -51,7 +67,7 @@ int main(){
 	}
 	
 	
-	
+	salida << "-1" << endl;
 	salida.close();
 
 }
