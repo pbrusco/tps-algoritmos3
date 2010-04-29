42c7a86973ad8d6f934ca29b372b1d458fa99485
pablo.brusco <pablo.brusco@2f5f89bf-aa3b-9c9d-ef5c-1903fc5e024b>
2010-04-29 20:06:19 +0000

    compila de en serio

--- TP2/ej2/posible.cpp
+++ TP2/ej2/posible.cpp
@@ -21,14 +21,14 @@ struct Grafo{
  	map<int,datos> info;	//DADO UN VERTICE, DA LA INFO (PADRE-VECINOS-EJESHASTA-VISITADO)
 	vector<vector<int> > matrizAdyNombrada;	//es una matriz de adyacencia donde en las intersecciones tiene el nombre del eje que los une.
 
-} //Ver como inicializar
+}; //Ver como inicializar
 
 
 void insertarResta(set<int>& ejesUsados,const set<int>& vEjesHasta,const set<int>& wEjesHasta);	
 int eje(const Grafo& G, int v1,int v2); //O(1)
 bool cargar(Grafo &G,ifstream &entrada);
 void limpiar(Grafo &G);
-
+int dfs_ciclos(int vertice,Grafo &G);
 
 
 int main(){
@@ -47,7 +47,7 @@ int main(){
 		n = G.info.size();
 		
 		if(sigueArchivo){ 
-			k = dfs_ciclos(1);
+			k = dfs_ciclos(1,G);
 			if(k < n || G.ejesUsados.size() < G.cant_aristas) salida<< "no" << endl;}
 			else salida << "fuertemente conexo" << endl;
 
@@ -65,23 +65,23 @@ int main(){
 
 
 
-int dfs_ciclos(int vertice){ //ESTO VA A SER LLAMADO K VECES CON K<=n)
+int dfs_ciclos(int vertice,Grafo &G){ //ESTO VA A SER LLAMADO K VECES CON K<=n)
 
-	G.info[vertice].ejesHasta = G.info[G.info[padre]].ejesHasta  // Linear on sizes (destruction, copy construction). O(n).(recordar que los ejes hasta son la misma cant que los nodos visitados)
-	G.info[vertice].ejesHasta.insert(eje(v,G.info[vertice].padre)); //( set.insert(x) ), logarithmic. + C(eje(v1,v2))
+	G.info[vertice].ejesHasta = G.info[G.info[vertice].padre].ejesHasta;  // Linear on sizes (destruction, copy construction). O(n).(recordar que los ejes hasta son la misma cant que los nodos visitados)
+	G.info[vertice].ejesHasta.insert(eje(G,vertice,G.info[vertice].padre)); //( set.insert(x) ), logarithmic. + C(eje(v1,v2))
 	G.info[vertice].visitado = true;	//O(1)
 	
 	for(set<int>::iterator it = G.info[vertice].vecinos.begin(); it != G.info[vertice].vecinos.end();it++){ //Recorre los vecinos del vertice actual //Puede ejecutarse hasta n-1 veces
 	
 		if(not G.info[*it].visitado){				//Si el vecino no esta visitado (O(1))
 			G.info[*it].padre = vertice;			//O(1)
-			return dfs_ciclos(*it) + 1;				//lo visito
+			return dfs_ciclos(*it,G) + 1;				//lo visito
 		}
 		else{							//Si el vecino esta visitado
 		
 			if(G.info[vertice].padre != (*it)){		//si no es el padre, entonces tengo un circuito O(1)
 				insertarResta(G.ejesUsados,G.info[vertice].ejesHasta,G.info[*it].ejesHasta);		//O(n.log n) (VER)	
-				G.ejesUsados.insert(eje(*it,vertice));		//O(log() + C(eje());
+				G.ejesUsados.insert(eje(G,*it,vertice));		//O(log() + C(eje());
 			}
 		}
 		
@@ -115,11 +115,11 @@ void insertarResta(set<int>& ejesUsados,const set<int>& vEjesHasta,const set<int
 bool cargar(Grafo &G,ifstream &entrada){
 
 	int k = 1;
-	int n,a;
+	int n,cantVecinos,vecino;
 	limpiar(G);
 	entrada >> n;
-	matrizAdyNombrada.rezise(n);
-	for(int s = 0;s<n;s++) 	matrizAdyNombrada[s].rezise(n);
+	G.matrizAdyNombrada.resize(n);
+	for(int s = 0;s<n;s++) 	G.matrizAdyNombrada[s].resize(n);
 
 		if (n == -1) return false;
 		
@@ -127,7 +127,7 @@ bool cargar(Grafo &G,ifstream &entrada){
 			entrada >> cantVecinos;
 			for(int j = 1;j<= cantVecinos;j++){
 				entrada >> vecino;
-				if(G.matrizAdyNombrada[i-1][vecino-1] == 0) {G.matrizAdyNombrada[vecino-1][i-1] = k;k++;G.cant_aristas++}
+				if(G.matrizAdyNombrada[i-1][vecino-1] == 0) {G.matrizAdyNombrada[vecino-1][i-1] = k;k++;G.cant_aristas++;}
 				else G.matrizAdyNombrada[vecino-1][i-1] = G.matrizAdyNombrada[i-1][vecino-1];
 				G.info[i].vecinos.insert(vecino);
 			}
@@ -141,9 +141,9 @@ bool cargar(Grafo &G,ifstream &entrada){
 
 
 void limpiar(Grafo &G){
-	G.ejesUsados.clear()
+	G.ejesUsados.clear();
  	G.info.clear();
-	matrizAdyNombrada.rezise(0);	
+	G.matrizAdyNombrada.resize(0);	
 	G.cant_aristas = 0;
 }
 
