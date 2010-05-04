96fb4afb52f67644fcf1db8b3ce04722cb5f4201
pablo.brusco <pablo.brusco@2f5f89bf-aa3b-9c9d-ef5c-1903fc5e024b>
2010-05-04 06:04:51 +0000

    ej1

--- trunk/TP2/ej1/generador/generador.cpp
+++ trunk/TP2/ej1/generador/generador.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
-#include <time.h>
-
-#include "../carcel.h"
+#include <fstream>
+#include <stdlib.h>
 
 using namespace std;
+
+
+int numeroAlAzar(int desde, int hasta, int x);
+
+
+
+
+int main(){
+	ofstream salida;
+	salida.open("Tp2Ej1.in");
+	
+	for(int tamSecuencia = 1;tamSecuencia < 250;tamSecuencia++){
+		salida << tamSecuencia << " ";
+		for(int j = 0;j<tamSecuencia;j++){		
+			salida << numeroAlAzar(-1000000, 1000000, 0)<<" ";			
+		}
+		salida << endl;
+	}
+		
+	salida << -1;
+
+	salida.close();
+	return 0;
+}
+
+
+int numeroAlAzar(int desde, int hasta, int x){
+
+	int a = x;
+	while(a == x){
+		a = (rand() % (hasta-desde)) + desde;
+	}
+
+	return a;
+}
+
