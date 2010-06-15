ff3f05726ad71dbf997ddfba03d4eae0f29a8878
marianobianchi08 <marianobianchi08@2f5f89bf-aa3b-9c9d-ef5c-1903fc5e024b>
2010-06-15 15:20:32 +0000

    Idea minima del ejercicio 4

---
+++ TP3/ej4/ej4.h
@@ -0,0 +1,31 @@
+#ifndef __EJ1__
+#define __EJ1__
+
+#include <vector>
+#include <set>
+
+using namespace std;
+
+struct Grafo{
+	//atributos
+	vector<vector<bool> > matAd;
+	
+	//metodos
+	int cantVecinos(int v, const set<int>& vecindad) const;
+	bool sonAdyacentes(int v1, int v2) const;
+	bool vecinoDeTodos(int v, const set<int>& c) const;
+	void cargar(istream& is, int cantNodos);
+	set<int> fronteraEnComun(int u, int v) const;
+	set<int> losMasFronterizos(int &u, int &v) const;
+	int masRelacionado(const set<int>& frontera) const;
+	set<int> cliqueConstructivo() const;
+	void busquedaLocal(set<int> &cliqueMaximo) const;
+};
+
+void guardarRes(ostream& os, set<int> c);
+
+
+
+
+
+#endif
