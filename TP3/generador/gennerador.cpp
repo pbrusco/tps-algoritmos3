8b06bdc0ce3d351af531717f7a0db5ca78a54595
pablo.brusco@gmail.com <pablo.brusco@gmail.com@2f5f89bf-aa3b-9c9d-ef5c-1903fc5e024b>
2010-06-25 14:09:10 +0000

    terminar generador

---
+++ TP3/generador/gennerador.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <time.h>
+
+int numeroAlAzar(int desde, int hasta);
+void generarGrafoAlAzar();
+
+
+using namespace std;
+
+
+struct Grafo {
+
+	vector<vector<int> > matrizAd;
+	Grafo(int V);
+	
+	void guardarnuestro(ostream& os);
+
+};
+
+
+
+int main(){
+
+	generarGrafoAlAzar();
+	
+	return 0;
+}
+
+Grafo::Grafo(int V){
+	matrizAd.rezise(0);
+	matrizAd.resize(V);
+	for(int i = 0; i<V;i++) matrizAd[i].resize(V);
+}
+
+void Grafo::guardarnuestro(ostream& os){
+
+	os << matrizAd.size() << endl;
+	for(int i = 0; i < matrizAd.size(); i++){
+	
+		int aristasPorCol = 0;
+		for(int j = 0; j < matrizAd.size();j++){
+			if (matrizAd[i][j] != 0) aristasPorCol++;
+		}
+		
+		os << '\t' << aristasPorCol << '\t';
+		
+		for(int j = 0; j < matrizAd.size();j++){
+			if (matrizAd[i][j] != 0) os << matrizAd[i][j]+1 << " ";
+
+		}
+		
+		
+		
+		os << endl;	
+	}
+	
+}
+
+
+int numeroAlAzar(int desde, int hasta){
+	return (rand() % (hasta-desde)) + desde;
+}
+
+
+
+void generarGrafoAlAzar(){
+
+	int cantNodos,k,cantEjes,probabilidadEje;
+	
+	string nombreOut;
+	
+	ofstream os;
+	cout << "ingrese el nombre con el que quiere guardar el archivo ";
+	cin.ignore();
+	getline(cin,nombreOut);
+	os.open(nombreOut.c_str());
+	
+	cout 	<< "Ingresar:" << endl
+			<< "Cantidad de nodos maxima (se van a crear grafos desde 5 nodos hasta lo que pongas aca.. multiplos de 5) ";
+	cin >> cantNodosMax;
+	
+	cout << "Probabilidad que aparezca un eje (de 0 a 100) ";
+	cin >> probabilidadEje;
+	
+	probabilidadEje = probabilidadEje % 101; //guardo el porcentaje
+	
+	for(int n = 5; n <= cantNodosMax;n++){
+		Grafo g(n);
+		llenarAristas(probabilidadEje,g);
+		g.guardarnuestro(os);
+	}
+
+	
+	
+	
+	os << -1;
+	os << endl << endl << "Probabilidad Ejes " << probabilidadEje;
+	os.close();
+
+}
+
