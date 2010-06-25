6d9806f9570b66d29aa6d63470e6232c84aa38e9
marianobianchi08 <marianobianchi08@2f5f89bf-aa3b-9c9d-ef5c-1903fc5e024b>
2010-06-25 18:04:18 +0000

    Listo el generador

--- trunk/TP3/generador/gennerador.cpp
+++ trunk/TP3/generador/gennerador.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <vector>
 #include <time.h>
 
@@ -16,20 +18,22 @@ struct Grafo {
 	Grafo(int V);
 	
 	void guardarnuestro(ostream& os);
+	void llenarAristas(int);
+	
 
 };
 
 
 
 int main(){
-
+	srand(time(NULL));
 	generarGrafoAlAzar();
 	
 	return 0;
 }
 
 Grafo::Grafo(int V){
-	matrizAd.rezise(0);
+	matrizAd.clear();//ESTO LO DEJA CON TAMAÑO CERO Y DESTRUYE A LOS ELEMENTOS DE ADENTRO
 	matrizAd.resize(V);
 	for(int i = 0; i<V;i++) matrizAd[i].resize(V);
 }
@@ -47,7 +51,8 @@ void Grafo::guardarnuestro(ostream& os){
 		os << '\t' << aristasPorCol << '\t';
 		
 		for(int j = 0; j < matrizAd.size();j++){
-			if (matrizAd[i][j] != 0) os << matrizAd[i][j]+1 << " ";
+			//if (matrizAd[i][j] != 0) os << matrizAd[i][j]+1 << " "; //IMAGINO QUE QUISISTE PONER LO QUE SIGUE
+			if (matrizAd[i][j] != 0) os << j+1 << " ";
 
 		}
 		
@@ -67,18 +72,17 @@ int numeroAlAzar(int desde, int hasta){
 
 void generarGrafoAlAzar(){
 
-	int cantNodos,k,cantEjes,probabilidadEje;
+	int cantNodosMax,k,cantEjes,probabilidadEje;
 	
 	string nombreOut;
 	
 	ofstream os;
 	cout << "ingrese el nombre con el que quiere guardar el archivo ";
-	cin.ignore();
 	getline(cin,nombreOut);
 	os.open(nombreOut.c_str());
 	
 	cout 	<< "Ingresar:" << endl
-			<< "Cantidad de nodos maxima (se van a crear grafos desde 5 nodos hasta lo que pongas aca.. multiplos de 5) ";
+			<< "Cantidad de nodos maxima (se van a crear grafos desde 5 nodos hasta lo que pongas aca.. multiplos de 5) ";//por que multiplos de 5?
 	cin >> cantNodosMax;
 	
 	cout << "Probabilidad que aparezca un eje (de 0 a 100) ";
@@ -88,7 +92,7 @@ void generarGrafoAlAzar(){
 	
 	for(int n = 5; n <= cantNodosMax;n++){
 		Grafo g(n);
-		llenarAristas(probabilidadEje,g);
+		g.llenarAristas(probabilidadEje);
 		g.guardarnuestro(os);
 	}
 
@@ -99,5 +103,23 @@ void generarGrafoAlAzar(){
 	os << endl << endl << "Probabilidad Ejes " << probabilidadEje;
 	os.close();
 
+}
+
+void Grafo::llenarAristas(int probEje){
+
+	int moneda;
+
+	for(int i = 0; i < matrizAd.size(); i++){
+		for(int j = 0; j < matrizAd.size(); j++){
+			
+			moneda = rand() % 101;
+			moneda = moneda == 101? 0 : moneda;//para que quede entre 0 y 100
+			
+			if(moneda < probEje){
+				matrizAd[i][j] = 1;
+				matrizAd[j][i] = 1;
+			}	
+		}
+	}
 }
 
